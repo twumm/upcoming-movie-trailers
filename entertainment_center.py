781fa6b9a783082868e1cfb9c108af8b1f3c458c
twumm <martint.mensah@gmail.com>
2017-07-26 16:39:19 +0000

    initial version

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,13 +1,15 @@
-'''This file makes a request to themoviedb.org's api and fetches the following movie details:
-title, storyline/overview, poster image url, youtube trailer url and the release date
+'''This file makes a request to themoviedb.org's api and fetches the following
+movie details:
+title, storyline/overview, poster image url, youtube trailer url and the
+release date
 '''
 import media
 import fresh_tomatoes
 import requests
 
 # access the api and convert it to a json file
-MOVIE_LIST = requests.get("https://api.themoviedb.org/3/movie/upcoming?api_key=" +
-                          "03531cf9267cbb1ee25c65670346beca&page=1")
+MOVIE_LIST = requests.get("https://api.themoviedb.org/3/movie/upcoming?" +
+                          "api_key=03531cf9267cbb1ee25c65670346beca&page=1")
 MOVIE_DATA = MOVIE_LIST.json()
 MOVIE_TRAILER_BASE_URL = "https://api.themoviedb.org/3/movie/"
 MOVIE_TRAILER_END_URL = "/videos?api_key=03531cf9267cbb1ee25c65670346beca"
@@ -16,18 +18,21 @@ YOUTUBE_TRAILER_BASE_URL = "https://www.youtube.com/watch?v="
 MOVIES = []
 
 for item in MOVIE_DATA["results"]:
-    # Loop through the response received from api.themoviedb.org and pick out the
-    # title, overview and movie image
+    # Loop through the response received from api.themoviedb.org and pick out
+    # the title, overview and movie image
     movie_title = item["title"]
     movie_storyline = item["overview"][0:200].encode('utf8') + "...read more"
     poster_image = POSTER_BASE_URL + item["poster_path"]
     release_date = item["release_date"]
 
-    # Get the video feed for each movie item and assign the video URL to trailer_youtube
+    # Get the video feed for each movie item and assign the video URL to
+    # trailer_youtube
     movie_id = str(item["id"])
-    movie_trailer_list = requests.get(MOVIE_TRAILER_BASE_URL + movie_id + MOVIE_TRAILER_END_URL)
+    movie_trailer_list = requests.get(MOVIE_TRAILER_BASE_URL + movie_id +
+                                      MOVIE_TRAILER_END_URL)
     movie_trailer_data = movie_trailer_list.json()
-    trailer_youtube = YOUTUBE_TRAILER_BASE_URL + movie_trailer_data["results"][0]["key"]
+    trailer_youtube = YOUTUBE_TRAILER_BASE_URL \
+        + movie_trailer_data["results"][0]["key"]
 
     # Create an instance of the Movie() class using the items
     movie_instance = media.Movie(movie_title, movie_storyline, poster_image,
--- media.py
+++ media.py
@@ -2,20 +2,21 @@
 '''
 import webbrowser
 
+
 class Movie():
     '''This program stores details on movies and shows their trailers'''
-
-    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
-
-    # the __init__ function accepts 4 variables in order to instantiate a movie
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, release_date):
+    # the __init__ function accepts 5 variables in order to instantiate a movie
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube, release_date):
+        '''Accepts 5 variables in order to instantiate a movie'''
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
         self.release_date = release_date
 
-    # the show_trailer function opens the link to the movie which has been clicked by the user
+    # the show_trailer function opens the link to the movie which has been
+    # clicked by the user
     def show_trailer(self):
         '''Opens the trailer of the movie file'''
         webbrowser.open(self.trailer_youtube_url)
